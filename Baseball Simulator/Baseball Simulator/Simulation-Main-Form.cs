4077c4a0e65147a3c6c2f01670e6faac8a740b97
mwalker55 <mark.walker620@gmail.com>
2015-12-27 00:09:37 -0800

    Fixed bugs, added repeat functionality

--- Baseball Simulator/Baseball Simulator/Simulation-Main-Form.cs
+++ Baseball Simulator/Baseball Simulator/Simulation-Main-Form.cs
@@ -16,6 +16,7 @@ namespace Baseball_Simulator
         private Player[] team2Players;
         private Player team1Pitcher;
         private Player team2Pitcher;
+        private int playersSet;
         public Home_Page()
         {
             InitializeComponent();
@@ -23,145 +24,180 @@ namespace Baseball_Simulator
             team2Pitcher = new Player();
             team1Players = new Player[9];
             team2Players = new Player[9];
-            for(int i = 0; i < 9; i++)
+            for (int i = 0; i < 9; i++)
             {
                 team1Players[i] = new Player();
                 team2Players[i] = new Player();
             }
-        }
-
-        
-        private void button1_Click(object sender, EventArgs e)
-        {
-            
+            playersSet = 0;
         }
 
         private void team1Player1_Click(object sender, EventArgs e)
         {
             team1Players[0].ShowDialog();
             team1Player1.Text = team1Players[0].name;
+            playersSet++;
         }
 
         private void team1Player2_Click(object sender, EventArgs e)
         {
             team1Players[1].ShowDialog();
             team1Player2.Text = team1Players[1].name;
+            playersSet++;
         }
 
         private void team1Player3_Click(object sender, EventArgs e)
         {
             team1Players[2].ShowDialog();
             team1Player3.Text = team1Players[2].name;
+            playersSet++;
         }
 
         private void team1Player4_Click(object sender, EventArgs e)
         {
             team1Players[3].ShowDialog();
             team1Player4.Text = team1Players[3].name;
+            playersSet++;
         }
 
         private void team1Player5_Click(object sender, EventArgs e)
         {
             team1Players[4].ShowDialog();
             team1Player5.Text = team1Players[4].name;
+            playersSet++;
         }
 
         private void team1Player6_Click(object sender, EventArgs e)
         {
             team1Players[5].ShowDialog();
             team1Player6.Text = team1Players[5].name;
+            playersSet++;
         }
 
         private void team1Player7_Click(object sender, EventArgs e)
         {
             team1Players[6].ShowDialog();
             team1Player7.Text = team1Players[6].name;
+            playersSet++;
         }
 
         private void team1Player8_Click(object sender, EventArgs e)
         {
             team1Players[7].ShowDialog();
             team1Player8.Text = team1Players[7].name;
+            playersSet++;
         }
 
         private void team1Player9_Click(object sender, EventArgs e)
         {
             team1Players[8].ShowDialog();
             team1Player9.Text = team1Players[8].name;
+            playersSet++;
         }
 
         private void team2Player1_Click(object sender, EventArgs e)
         {
             team2Players[0].ShowDialog();
             team2Player1.Text = team2Players[0].name;
+            playersSet++;
         }
 
         private void team2Player2_Click(object sender, EventArgs e)
         {
             team2Players[1].ShowDialog();
             team2Player2.Text = team2Players[1].name;
+            playersSet++;
         }
 
         private void team2Player3_Click(object sender, EventArgs e)
         {
             team2Players[2].ShowDialog();
             team2Player3.Text = team2Players[2].name;
+            playersSet++;
         }
 
         private void team2Player4_Click(object sender, EventArgs e)
         {
             team2Players[3].ShowDialog();
             team2Player4.Text = team2Players[3].name;
+            playersSet++;
         }
 
         private void team2Player5_Click(object sender, EventArgs e)
         {
             team2Players[4].ShowDialog();
             team2Player5.Text = team2Players[4].name;
+            playersSet++;
         }
 
         private void team2Player6_Click(object sender, EventArgs e)
         {
             team2Players[5].ShowDialog();
             team2Player6.Text = team2Players[5].name;
+            playersSet++;
         }
 
         private void team2Player7_Click(object sender, EventArgs e)
         {
             team2Players[6].ShowDialog();
             team2Player7.Text = team2Players[6].name;
+            playersSet++;
         }
 
         private void team2Player8_Click(object sender, EventArgs e)
         {
             team2Players[7].ShowDialog();
             team2Player8.Text = team2Players[7].name;
+            playersSet++;
         }
 
         private void team2Player9_Click(object sender, EventArgs e)
         {
             team2Players[8].ShowDialog();
             team2Player9.Text = team2Players[8].name;
+            playersSet++;
         }
 
         private void team1PitcherButton_Click(object sender, EventArgs e)
         {
             team1Pitcher.ShowDialog();
             team1PitcherButton.Text = team1Pitcher.name;
+            playersSet++;
         }
 
         private void team2PitcherButton_Click(object sender, EventArgs e)
         {
             team2Pitcher.ShowDialog();
             team2PitcherButton.Text = team2Pitcher.name;
+            playersSet++;
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            Simulator game = new Simulator(team1Players, team2Players, team1Pitcher, team2Pitcher);
-            Tuple<int, int> result = game.simulateGames(50);
-            MessageBox.Show("Team 1 won " + result.Item1 + " games!  Average innings per game was " + result.Item2/50);
-            this.Close();
+            if (playersSet < 20)
+            {
+                MessageBox.Show("Error: Not every player has been set!", "Error");
+                return;
+            }
+            Simulator game = new Simulator(deepCopyArray(team1Players), deepCopyArray(team2Players), team1Pitcher, team2Pitcher);
+            Tuple<int, int> result = game.simulateGames(1000);
+            MessageBox.Show("Team 1 won " + result.Item1 + " games!  Average innings per game was " + result.Item2/1000);
+        }
+
+        private Player[] deepCopyArray(Player[] toBeCopied)
+        {
+            Player[] clonedArray = new Player[toBeCopied.Length];
+            for(int i = 0; i < toBeCopied.Length; i++)
+            {
+                clonedArray[i] = new Player();
+                clonedArray[i].name = toBeCopied[i].name;
+                clonedArray[i].walk_percentage = toBeCopied[i].walk_percentage;
+                clonedArray[i].single_percentage = toBeCopied[i].single_percentage;
+                clonedArray[i].double_percentage = toBeCopied[i].double_percentage;
+                clonedArray[i].triple_percentage = toBeCopied[i].triple_percentage;
+                clonedArray[i].HR_percentage = toBeCopied[i].HR_percentage;
+            }
+            return clonedArray;
         }
     }
 }
