cd9f57ebcde86f22eac0b28a8eca5247fcf063a8
mwalker55 <mark.walker620@gmail.com>
2015-12-31 22:02:09 -0800

    Working toward League addition

---
+++ Baseball Simulator/Baseball Simulator/League.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baseball_Simulator
+{
+    class League
+    {
+        private List<Team> leagueTeams;
+
+        public League()
+        {
+            leagueTeams = new List<Team> { };
+        }
+
+        public void addTeamToLeague(Team toBeAdded)
+        {
+            leagueTeams.Add(toBeAdded);
+        }
+    }
+}
---
+++ Baseball Simulator/Baseball Simulator/Team.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baseball_Simulator
+{
+    class Team
+    {
+        private Player[] playerList;
+        public Player startingPitcher { get; set; }
+        public Team()
+        {
+            playerList = new Player[9];
+            startingPitcher = new Player();
+        }
+
+        public void addPlayerToRoster(Player toBeAdded, int lineUpPosition)
+        {
+            playerList[lineUpPosition - 1] = toBeAdded;
+        }
+
+        public Player getPlayerAtLineupPosition(int lineUpPosition)
+        {
+            return playerList[lineUpPosition - 1];
+        }
+    }
+}
